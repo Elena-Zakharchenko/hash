43fc288a486c90c02151e2d8b09765eb49d0ec43
Elena-Zakharchenko <37251748+Elena-Zakharchenko@users.noreply.github.com>
2018-11-27 01:27:53 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <math.h>
+#include <string>
+#include <fstream> 
+#include <iomanip> 
+#include <cstring>
+#include <vector>           
+#include <sstream>          
+#include <algorithm>    
+#include "repository.h"
+#include <list>
+#include <iterator>
+
+int main()
+{
+	Repository r;
+	r.createHash();
+	r.readfile();
+	return 0;
+}
